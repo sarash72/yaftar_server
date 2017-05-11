53c7a1a7ab56862be3cc8fb197cf37a69def510d
sara <sara.shirdel72@yahoo.com>
2017-05-11 16:36:52 +0430

    save data in db in server class

--- src/yaftar/Server.java
+++ src/yaftar/Server.java
@@ -11,36 +11,31 @@ import java.lang.reflect.Modifier;
 import java.lang.management.ManagementFactory;
 
 
-class Threads extends Thread {
-	public void run(){
-		 OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-		  for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) {
-		    method.setAccessible(true);
-		    if (method.getName().startsWith("get") 
-		        && Modifier.isPublic(method.getModifiers())) {
-		            Object value;
-		        try {
-		            value = method.invoke(operatingSystemMXBean);
-		        } catch (Exception e) {
-		            value = e;
-		        } // try
 
-		        System.out.println(method.getName() + " = " + value);}
-		    } // if
-		  } // forsdsd
-		
-	}
-
-
+// saver class(main)
 public class Server {
+	private static SessionFactory factory;
+
 	public static void main(String[] args) throws IOException {
+		// save data in database with hibernate
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		Session session = factory.openSession();
+		
+		
+		
+
+		
+		// sarversocket
 		ServerSocket server = new ServerSocket(12324);
 		Socket client = server.accept();
 
 		DataInputStream is = new DataInputStream(client.getInputStream());
 		DataOutputStream os = new DataOutputStream(client.getOutputStream());
-		Threads thread=new Threads();
-		thread.start();
 		System.out.println("sarver");
 	}
 
