60223605a92b704f274ecae8b33a64c3a2068687
sara <sara.shirdel72@yahoo.com>
2017-05-11 16:38:53 +0430

    save data every 1 min in databases

--- src/yaftar/Server.java
+++ src/yaftar/Server.java
@@ -5,6 +5,16 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import java.lang.management.OperatingSystemMXBean;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -27,7 +37,22 @@ public class Server {
 		Session session = factory.openSession();
 		
 		
-		
+		// get date every 1 mine
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			public void run() {
+				Date date = new Date();
+				SimpleDateFormat ft = new SimpleDateFormat("yyyy.MM.dd kk:mm");
+				String string_date = ft.format(date).toString();
+				String[] date_split = string_date.split(" ");
+				Transaction tx = null;
+				Integer sourceID = null;
+				Source source = new Source(date_split[0], date_split[1], "1", "2", "3", "4");
+				tx = session.beginTransaction();
+				sourceID = (Integer) session.save(source);
+				tx.commit();
+			}
+		}, 0, 1000 * 60 * 1);
 
 		
 		// sarversocket
