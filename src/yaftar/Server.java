8f614c83224a73e1bf3295f94c58cfd8daa263d6
sara <sara.shirdel72@yahoo.com>
2017-05-11 17:49:41 +0430

    save memory-cpu-disk data in databases

--- src/yaftar/Server.java
+++ src/yaftar/Server.java
@@ -20,7 +20,25 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.management.ManagementFactory;
 
+class Threads extends Thread {
+	public void run() {
+		OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+		for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) {
+			method.setAccessible(true);
+			if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
+				Object value;
+				try {
+					value = method.invoke(operatingSystemMXBean);
+				} catch (Exception e) {
+					value = e;
+				} // try
 
+				System.out.println(method.getName() + " = " + value);
+			}
+		} // if
+	} // for
+
+}
 
 // saver class(main)
 public class Server {
@@ -41,13 +59,38 @@ public class Server {
 		Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
 			public void run() {
+				String memory = null,disk = null,cpu = null;
 				Date date = new Date();
 				SimpleDateFormat ft = new SimpleDateFormat("yyyy.MM.dd kk:mm");
 				String string_date = ft.format(date).toString();
 				String[] date_split = string_date.split(" ");
 				Transaction tx = null;
 				Integer sourceID = null;
-				Source source = new Source(date_split[0], date_split[1], "1", "2", "3", "4");
+				//get system sources data
+
+				 OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+				  for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) {
+				    method.setAccessible(true);
+				    if (method.getName().startsWith("get") 
+				        && Modifier.isPublic(method.getModifiers())) {
+				            Object value;
+				        try {
+				            value = method.invoke(operatingSystemMXBean);
+				        } catch (Exception e) {
+				            value = e;
+				        } // try
+		if(method.getName().equals("getCommittedVirtualMemorySize")){
+			memory=value.toString();}
+		if(method.getName().equals("getProcessCpuTime")){
+			 cpu=value.toString();}
+		if(method.getName().equals("getTotalPhysicalMemorySize")){
+			 disk=value.toString();}
+		
+				        //System.out.println(memory);
+				    } // if
+				  } // for
+				  
+				Source source = new Source(date_split[0], date_split[1], disk, memory, cpu, "4");
 				tx = session.beginTransaction();
 				sourceID = (Integer) session.save(source);
 				tx.commit();
