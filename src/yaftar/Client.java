b0c63b9f33d9c2f1e83ce904e346dbd42fd1a382
sara <sara.shirdel72@yahoo.com>
2017-05-16 02:19:34 +0430

    creat log file

--- src/yaftar/Client.java
+++ src/yaftar/Client.java
@@ -5,15 +5,57 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
+/**
+ * client class for get date and time
+ * 
+ * @author SHirdel
+ */
 public class Client {
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		
-			Socket client = new Socket("192.168.1.127", 12324);
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		Socket client = null;
+		try {
+			Logger logger = Logger.getLogger("MyLog");  
+		    FileHandler fh; 
+			client = new Socket("localhost", 12324);
+			/**
+			 * give date
+			 */
+			System.out.println("plz enter date same 2017.05.11");
+			String date = scanner.next();
+			/**
+			 * give time
+			 */
+			System.out.println("plz enter time same 19:02");
+			String time = scanner.next();
 
-			DataInputStream is = new DataInputStream(client.getInputStream());
+			/**
+			 * send date to server
+			 */
 			DataOutputStream os = new DataOutputStream(client.getOutputStream());
-	System.out.println("client");
+			//os.writeBytes(date + " " + time);
+			/**
+			 * get and print system information
+			 */
+			DataInputStream is = new DataInputStream(client.getInputStream());
+			//String input = is.readLine();
+			//System.out.println(input);
+			 fh = new FileHandler("D:/log/MyLogFile.log");  
+		        logger.addHandler(fh);
+		        SimpleFormatter formatter = new SimpleFormatter();  
+		        fh.setFormatter(formatter); 
 
-}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+
+		}
+
+	}
 }
